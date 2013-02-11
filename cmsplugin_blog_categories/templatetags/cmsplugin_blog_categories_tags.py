49d10fbb850d8c05503e83214ca6cd753f916de2
Martin Brochhaus <mbrochh@gmail.com>
2013-02-11 13:10:03 +0800

    Added exclude_empty parameter to render_category_links templatetag.
    Additionally you can display the number of posts by using `item.num_posts`
    in the snippet template.

--- cmsplugin_blog_categories/templatetags/cmsplugin_blog_categories_tags.py
+++ cmsplugin_blog_categories/templatetags/cmsplugin_blog_categories_tags.py
@@ -1,5 +1,6 @@
 """Templatetafs for the ``cmsplugin_blog_categories`` app."""
 from django import template
+from django.db.models import Count
 
 from cmsplugin_blog_categories.models import Category, EntryCategory
 
@@ -15,9 +16,10 @@ def get_category(entry):
 @register.inclusion_tag(
     'cmsplugin_blog_categories/category_links_snippet.html',
     takes_context=True)
-def render_category_links(context):
+def render_category_links(context, exclude_empty=False):
     """Renders a list of all categories in the database."""
-    context.update({
-        'categories': Category.objects.all()
-    })
+    qs = Category.objects.annotate(num_posts=Count('entrycategory'))
+    if exclude_empty:
+        qs = qs.exclude(num_posts=0)
+    context.update({'categories': qs, })
     return context
